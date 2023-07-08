d5804be71542a8ee529732e5c319105211f131a9
Julio Bermudez <julioph0n3@gmail.com>
2023-07-08 00:17:54 -0400

    added transition with display none

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,25 @@
 var btnStartEl = document.querySelector("#btnStart");
-var timerEl = document.querySelector("#timer")
+var timerEl = document.querySelector("#timer");
+var hideStartEl = document.querySelector("#startContainer");
+var hideQuestionsEl = document.querySelector("#questions");
 
 var secondsLeft = 50
 
-btnStartEl.addEventListener("click", start );
-
-function start(){
+btnStartEl.addEventListener("click", timerCountDown);
+btnStartEl.addEventListener("click", displayQuestions);
+//Start the Count Down
+function timerCountDown(){
     var timerInterval = setInterval(function(){
         secondsLeft--;
         timerEl.textContent = secondsLeft + " Seconds Left";
         if (secondsLeft === 0) {
             clearInterval(timerInterval);
-            //Time out missing code here
+            //When time is out something happend missing code here
         }
-    },1000)
+    },1000);
 }
-
+//Hide the Start Container and Show the Questions Container
+function displayQuestions(){
+    hideStartEl.setAttribute("style", "display:none");
+    hideQuestionsEl.setAttribute("style", "display: flex")
+}
